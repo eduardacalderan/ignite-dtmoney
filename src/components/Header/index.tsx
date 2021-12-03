b91d55ca2a55c3ddf7db6833136c3e7279ec6979
Eduarda Calderan <eduardanicolodicalderan@gmail.com>
2021-12-03 16:52:40 -0300

    alteração no services

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,3 @@
-import { ComponentProps, useState } from "react";
-import Modal from "react-modal";
 import logoImg from "../../assets/logo.svg";
 import { Container, Content } from "./style";
 
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000/api", //endereço que se repete em todas as requisições
+  baseURL: "https://eduardacalderan.github.io/ignite-reactjs-dtmoney/", //endereço que se repete em todas as requisições
 });
